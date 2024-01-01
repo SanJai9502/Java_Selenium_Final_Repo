fbf3222af9fdab2ca4309b54689d30667bd492c4
Puza Pant <surenpooja49@gmail.com>
2024-01-01 10:49:00 -0500

    first commite

---
+++ LearningJava/src/z_openQuestion_andNotes/rough_Paper.java
@@ -0,0 +1,13 @@
+package z_openQuestion_andNotes;
+
+public class rough_Paper {
+	static void surenMethod (){
+	for (int a=1; a<=5; a++);
+	System.out.println("one of the number is ");
+	}
+	public static void main(String[] args) {
+surenMethod();
+
+	}	
+	}
+
